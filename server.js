c6caad4053e24474a9d57fb2fe394cc371639974
KsawierCz <ksawiercz1222@gmail.com>
2025-06-17 15:35:26 +0200

    removing unnecessary line of code

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.set("views", path.join(__dirname, 'views'));
-
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render("index");
